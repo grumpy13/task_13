de42dbd5ea7065e8d8bc1473c1d396bba6db5aa1
Mariam Ali <mariamqali13@gmail.com>
2018-10-15 12:30:14 +0300

    FIXED 2.0

--- task_13/settings.py
+++ task_13/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'task_13.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': '13c53d6ab211cb391ead14bb5a047cf5',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
